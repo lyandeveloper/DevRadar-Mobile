3d4ef7a261aaec46b68c15bb8c7cf9a6ffb30ddd
lyandeveloper <tutors3d@gmail.com>
2020-01-16 13:19:03 -0300

    functionalities of app done

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -15,9 +15,13 @@ import {
   getCurrentPositionAsync,
 } from 'expo-location';
 
+import api from '../services/api';
+
 // import { Container } from './styles';
 
 export default function Main({ navigation }) {
+  const [techs, setTechs] = useState('');
+  const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null);
   useEffect(() => {
     async function loadInitialPosition() {
@@ -42,38 +46,60 @@ export default function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get('/search', {
+      params: {
+        latitude,
+        longitude,
+        techs,
+      },
+    });
+
+    setDevs(response.data.devs);
+  }
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region);
+  }
   if (!currentRegion) {
     return null;
   }
   return (
     <>
-      <MapView initialRegion={currentRegion} style={styles.map}>
-        <Marker coordinate={{ latitude: -3.854503, longitude: -40.926782 }}>
-          <Image
-            style={styles.avatar}
-            source={{
-              uri:
-                'https://avatars0.githubusercontent.com/u/17800360?s=460&v=4',
-            }}
-          />
-
-          <Callout
-            onPress={() => {
-              navigation.navigate('Profile', {
-                github_username: 'lyandeveloper',
-              });
+      <MapView
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion}
+        style={styles.map}>
+        {devs.map(dev => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              latitude: dev.location.coordinates[1],
+              longitude: dev.location.coordinates[0],
             }}>
-            <View style={styles.callout}>
-              <Text style={styles.devName}>Elian Campos</Text>
-              <Text style={styles.devBio}>
-                Amante das tecnologias Web e Mobile
-              </Text>
-              <Text style={styles.devTechs}>
-                ReactJS, React Native, Node.js
-              </Text>
-            </View>
-          </Callout>
-        </Marker>
+            <Image
+              style={styles.avatar}
+              source={{
+                uri: dev.avatar_url,
+              }}
+            />
+
+            <Callout
+              onPress={() => {
+                navigation.navigate('Profile', {
+                  github_username: dev.github_username,
+                });
+              }}>
+              <View style={styles.callout}>
+                <Text style={styles.devName}>{dev.name}</Text>
+                <Text style={styles.devBio}>{dev.bio}</Text>
+                <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
 
       <View style={styles.searchForm}>
@@ -83,9 +109,11 @@ export default function Main({ navigation }) {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
         />
 
-        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
           <MaterialIcons name="my-location" size={20} color="#FFF" />
         </TouchableOpacity>
       </View>
